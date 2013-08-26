ce722a65ebab82953c74ddab32f8ec710662ffec
mbartolett <mattbartolett@gmail.com>
2013-08-25 20:03:05 -0400

    inital commit comment

---
+++ npoiTest/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace npoiTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_import = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.cboSheets = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(557, 441);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_import
+            // 
+            this.btn_import.Location = new System.Drawing.Point(229, 519);
+            this.btn_import.Name = "btn_import";
+            this.btn_import.Size = new System.Drawing.Size(139, 32);
+            this.btn_import.TabIndex = 1;
+            this.btn_import.Text = "Import Deal Sheet";
+            this.btn_import.UseVisualStyleBackColor = true;
+            this.btn_import.Click += new System.EventHandler(this.btn_import_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(27, 477);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(97, 23);
+            this.btnOpenFile.TabIndex = 2;
+            this.btnOpenFile.Text = "Find that file";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // cboSheets
+            // 
+            this.cboSheets.FormattingEnabled = true;
+            this.cboSheets.Location = new System.Drawing.Point(337, 479);
+            this.cboSheets.Name = "cboSheets";
+            this.cboSheets.Size = new System.Drawing.Size(238, 21);
+            this.cboSheets.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 482);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Woah! More than one sheet, which one?";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(436, 519);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Create Final File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(337, 587);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(247, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(166, 594);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(165, 13);
+            this.lblDescription.TabIndex = 7;
+            this.lblDescription.Text = "Enter the description for the deals";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 634);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboSheets);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.btn_import);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_import;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.ComboBox cboSheets;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblDescription;
+        //private System.Data.DataSet dataSet1;
+    }
+}
+
---
+++ npoiTest/Form1.cs
@@ -0,0 +1,453 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.Util.Collections;
+
+namespace npoiTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Visible = false;
+            cboSheets.Visible = false;
+            lblDescription.Visible = false;
+            textBox1.Visible = false;
+        }
+
+        HSSFWorkbook workBook;
+        DataSet dataSet1 = new DataSet();
+
+        void InitializeWorkbook(string path)
+        {
+            using (FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                 workBook = new HSSFWorkbook(file);
+            }
+        }
+
+        void ConvertToDataTable()
+        {
+            //create sheet obj
+             ISheet sheet = workBook.GetSheetAt(cboSheets.SelectedIndex);
+            //dtRushMuthaFucker(sheet);
+            dataSet1.Tables.Add(dtRushMuthaFucker(sheet));
+            //return! dtrushmuthafucker does it all! i knew it could be done in one function
+            return;
+
+            #region im hiding this shit, cause dtrush ends all fuckers
+            getDealColumnsSheetLevel(sheet);
+
+            System.Collections.IEnumerator rows = sheet.GetRowEnumerator();
+            
+            //create datatable
+            DataTable dt = new DataTable();
+            
+            //add columns to datatable
+            for (int i = 0; i < 32; i++)
+            {
+                dt.Columns.Add(Convert.ToChar(((int)'A') + i).ToString());
+            }
+
+            //load worksheet into datatable
+            while (rows.MoveNext())
+            {
+                IRow row = (IRow)rows.Current;
+                DataRow dr = dt.NewRow();
+
+                for (int j = 0; j < row.LastCellNum; j++)
+                {
+                    ICell cell = row.GetCell(j);
+
+                    if (cell == null)
+                    {
+                        dr[j] = null;
+                    }
+                    else
+                    {
+                        dr[j] = cell.ToString();
+                    }
+                }
+                dt.Rows.Add(dr);                
+            }
+
+            int iCurrentSheetColumns = NumberofDealColumns(dt);
+            List<int> lCustomerNumbers = iCustNumbers(dt);
+
+            //create new datatable and copyin wanted columns
+            DataTable updatedTable = dt.DefaultView.ToTable(false, "G", "I", "J","K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "V");
+
+            //take out emtpy rows
+            updatedTable = updatedTable.Rows.Cast<DataRow>().Where(row => !row.ItemArray.All(field => field is System.DBNull || string.Compare((field as string).Trim(), string.Empty) == 0)).CopyToDataTable();
+
+            //create new table for rows to transfer
+            DataTable currentTable = new DataTable();
+            
+            for (int i = 0; i < 32; i++)
+            {
+                currentTable.Columns.Add(Convert.ToChar(((int)'A') + i).ToString());
+            }
+            //determine if the value in column g is an int
+            DataColumn col = updatedTable.Columns["G"];
+            foreach (DataRow row in updatedTable.Rows)
+            {
+                object value = row[col];
+                
+                //check if cell is null
+                if (value != DBNull.Value)
+                {
+                    //check if value is an int
+                    //first cast the value as a string
+                    string sValue = value.ToString().Trim();
+                    //find the index of the last space in the string
+                    Int32 iLastSpace = sValue.LastIndexOf(' ');
+                    //find the length of the string
+                    Int32 iLastChar = sValue.Length;
+                    //if neither of those were zero lets move on
+                    if (iLastSpace > 0 & iLastChar > 0)
+                    {
+                        //grab the last part of the string this is where your deal # lives
+                        string sLastValue = sValue.Substring(iLastSpace, iLastChar - iLastSpace).Trim();
+                        int num;
+                        //if it is not an int we delete the row.
+                        if (int.TryParse(sLastValue, out num))
+                        {
+                            row.SetField(col, sLastValue);
+                            //this is messy and not the best way but it works
+                            for (int j = 8; j < dt.Columns.Count; j++)
+                            {
+                                string sUpdate;
+                                sUpdate = dt.Rows[16][j].ToString().Trim();
+                                if (!string.IsNullOrEmpty(sUpdate))
+                                {
+                                    int iStart;
+                                    int iLast;
+                                    iStart = sUpdate.LastIndexOf(" ");
+                                    iLast = sUpdate.Length;
+                                    sUpdate = sUpdate.Substring(iStart, iLast - iStart).Trim();
+                                    sUpdate = sUpdate + " " + row[j - 7].ToString();
+                                    row.SetField(j - 7, sUpdate);
+                                }
+                            }
+                            currentTable.ImportRow(row);
+                            currentTable.AcceptChanges();
+                        }
+                        else
+                        {
+                            //was not a valid int, get rid of row.
+                            row.Delete();
+                            
+                        }
+                    }
+                    else
+                    {
+                        //was no space in this cell delete the row
+                        row.Delete();                        
+                    }
+                }
+                else 
+                {
+                    //value was null, delete the row
+                    row.Delete();
+                }
+            }
+            //have to tell the table to accept changes so all the deleted rows actually delete
+            updatedTable.AcceptChanges();        
+            //add it to the dataset
+            dataSet1.Tables.Add(currentTable);
+            #endregion
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_import_Click(object sender, EventArgs e)
+        {
+            //InitializeWorkbook(@"test.xls");
+            ConvertToDataTable();            
+            //have to autogenerate columns or set them up yourself... aint nobody got time for that
+            dataGridView1.AutoGenerateColumns = true;
+            //bind return table to datagridview, should show up
+            dataGridView1.DataSource = dataSet1.Tables[0];
+            
+        }
+
+        //pass the full datatable in to here and it will return the number of deal columns
+        private int NumberofDealColumns(DataTable dtReadColumns)
+        {
+            //set the counter above the try so when it throws error we return the last good column
+            int iCounter = 0;
+            try
+            {
+                //current dealID we are trying to read
+                int iDealNum = 1;
+                
+                for (int j = 8; iDealNum > 0; j++)
+                {
+                    //read the column header here
+                    string sUpdate;
+                    sUpdate = dtReadColumns.Rows[16][j].ToString().Trim();
+                    int iStart;
+                    int iLast;
+                    iStart = sUpdate.LastIndexOf(" ");
+                    iLast = sUpdate.Length;
+                    //it actually exits here when the substring fails due to negative number
+                    sUpdate = sUpdate.Substring(iStart, iLast - iStart).Trim();
+                    //if we got a value out of the cell, make sure it is an int and add it to the counter
+                    if (int.TryParse(sUpdate, out iDealNum))
+                    {
+                        iCounter++;
+                    }
+                    else
+                    {
+                        iDealNum = -1;
+                    }
+                }
+
+                //either way, return the counter we know how many deals we have in this sheet.
+                return iCounter;
+            }
+            catch
+            {
+                return iCounter;
+            }
+        }
+
+        private List<int> iCustNumbers(DataTable dtToScan)
+        {
+            List<int> iCustomers = new List<int>();
+            try
+            {
+                for (int j = 19; j < dtToScan.Rows.Count; j++)
+                {
+                    //read the column header here
+                    string sUpdate;
+                    sUpdate = dtToScan.Rows[j][6].ToString().Trim();
+                    if (!string.IsNullOrEmpty(sUpdate))
+                    {
+                        int iStart;
+                        int iLast;
+                        iStart = sUpdate.LastIndexOf(" ");
+                        iLast = sUpdate.Length;
+                        //it actually exits here when the substring fails due to negative number
+                        sUpdate = sUpdate.Substring(iStart, iLast - iStart).Trim();
+                        //if we got a value out of the cell, make sure it is an int and add it to the counter
+                        int iCustomerNum;
+                        if (int.TryParse(sUpdate, out iCustomerNum))
+                        {
+                            iCustomers.Add(iCustomerNum);
+                        }                        
+                    }
+                }
+                return iCustomers;
+            }
+            catch
+            {
+                return iCustomers;
+            }
+        }
+
+        private int getDealColumnsSheetLevel(ISheet sheet)
+        {
+            int iReturnVal = 0;
+            try
+            {
+                System.Collections.IEnumerator rows = sheet.GetRowEnumerator();               
+                IRow test = sheet.GetRow(16);
+                iReturnVal = test.LastCellNum - 8;              
+
+                return iReturnVal;
+            }
+            catch
+            {
+                return iReturnVal;
+            }
+        }
+
+        private DataTable dtRushMuthaFucker(ISheet sheet)
+        {
+            dataSet1.Tables.Clear();
+            //create datatable cause we have to fucking return a data table
+            DataTable dtMaster = new DataTable("bitchtits");
+            try
+            {
+                //had to make time for this.. fuck
+                dtMaster.Columns.Add("CustNumber", typeof(string));
+                dtMaster.Columns.Add("DealNumber", typeof(string));
+                dtMaster.Columns.Add("Quantity", typeof(int));
+
+                //get those motha fucking rows
+                System.Collections.IEnumerator rows = sheet.GetRowEnumerator();
+                //this row has the motha fucking deal columns bitch.. save that shit for later use
+                IRow DealRows = sheet.GetRow(16);
+                //count the number of fucking deals, so we can step through it later
+                int iColumnCount = DealRows.LastCellNum;
+
+                //start at row 19, cause thats where the fucking customers start, then go till the last fucking row
+                for (int counter = 19; counter < sheet.LastRowNum; counter++)
+                {
+                    //get current fucking customer row
+                    IRow cRow = sheet.GetRow(counter);
+                    string sTrim;
+                    //gotta try catch that shit, cause those blank rows fuck shit up
+                    try
+                    {
+                        sTrim = cRow.GetCell(6).ToString();
+                    }
+                    catch
+                    {
+                        //blank row blows the fuck up set trim to empty and move the fuck on
+                        sTrim = string.Empty;
+                    }
+
+                    if (!string.IsNullOrEmpty(sTrim))
+                    {
+                        //gotta get the customer number out of those two numbers, put on your magic hat its time for sorcery
+                        int iStart;
+                        int iLast;
+                        iStart = sTrim.LastIndexOf(" ");
+                        iLast = sTrim.Length;
+                        
+                        sTrim = sTrim.Substring(iStart, iLast - iStart).Trim();
+                        //if we got a value out of the cell, make sure it is an int and add it to the counter
+                        int iCustomerNum;
+                        //int tryparse is fucking awesome! 
+                        if (int.TryParse(sTrim, out iCustomerNum))                        
+                        {                                
+                            //ok wizard, we got the customer number, lets read some fucking values ruler style
+                            for (int columnStart = 8; columnStart < iColumnCount; columnStart++)
+                            {
+                                //again its time for string magic, cause the excel sheet is fucking stupid.. get the deal number bitch
+                                int iCurrentDealnum;
+                                string sCurrentDealCell;
+                                sCurrentDealCell = DealRows.GetCell(columnStart).ToString();
+                                int iStartDeal;
+                                int iLastDeal;
+                                iStartDeal = sCurrentDealCell.LastIndexOf(" ");
+                                iLastDeal = sCurrentDealCell.Length;                               
+                                sCurrentDealCell = sCurrentDealCell.Substring(iStartDeal, iLastDeal - iStartDeal).Trim();
+                                if(int.TryParse(sCurrentDealCell, out iCurrentDealnum))
+                                {
+                                    //we got us a mother fucking deal number, lets check the customer quantity for this deal
+                                    string sCurrentQty;
+                                    sCurrentQty = cRow.GetCell(columnStart).ToString();
+                                    //read the cell for our current fucking customer and our current fucking deal
+                                    int iCurrentQty;
+                                    if (int.TryParse(sCurrentQty, out iCurrentQty))
+                                    {
+                                        //fuck yeah ass muncher we got a valid quantity
+                                        if (iCurrentQty > 0)
+                                        {
+                                            //the quantity is bigger than 1.. add that shit to the table
+                                            DataRow drAdd = dtMaster.NewRow();
+                                            drAdd["CustNumber"] = iCustomerNum.ToString();
+                                            drAdd["DealNumber"] = iCurrentDealnum.ToString();
+                                            drAdd["Quantity"] = iCurrentQty;
+                                            dtMaster.Rows.Add(drAdd);
+                                            dtMaster.AcceptChanges();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                //return that table for fucks sake!
+                return dtMaster;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return dtMaster;
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            InitializeWorkbook(openFileDialog1.FileName);
+            int iCounter = 1;
+            
+            foreach (ISheet sheet in workBook)
+            {
+                cboSheets.Items.Add(sheet.SheetName);
+                iCounter++;
+            }          
+
+            if (iCounter > 1)
+            {
+                label1.Visible = true;
+                cboSheets.Visible = true;
+                cboSheets.SelectedIndex = 0;
+
+                lblDescription.Visible = true;
+                textBox1.Visible = true;
+            }
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            //saveFileDialog1.ShowDialog();
+            //clsStringBuilder stringbuilder = new clsStringBuilder();
+            //string sCSV = stringbuilder.ToCSV(dataSet1.Tables[0]);
+
+            string description = textBox1.Text;
+
+            string CsvFpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "FWDEAORD.csv");
+            try
+            {
+                StreamWriter csvFileWriter = new StreamWriter(CsvFpath, false);
+
+                int countColumn = dataGridView1.ColumnCount -1;
+
+                string docust = "DOCUST";
+                string doitem = "DOITEM";
+                string doqtyo = "DOQTYO";
+                string dodesc = "DODESC";
+
+                csvFileWriter.WriteLine(docust + ',' + doitem + ',' + doqtyo + ',' + dodesc);
+
+                foreach (DataGridViewRow dataRowObject in dataGridView1.Rows)
+                {
+                    if (!dataRowObject.IsNewRow)
+                    {
+                        string dataFormGrid = "";
+
+                        dataFormGrid = dataRowObject.Cells[0].Value.ToString();
+
+                        for (int i = 1; i <= countColumn; i++)
+                        {
+                            dataFormGrid = dataFormGrid + ',' + dataRowObject.Cells[i].Value.ToString();
+                            
+                        }
+                        dataFormGrid = dataFormGrid + ',' + description;
+                        csvFileWriter.WriteLine(dataFormGrid);
+                    }
+                }
+
+                csvFileWriter.Flush();
+                csvFileWriter.Close();
+            }
+            catch (Exception exceptionObject)
+            {
+                MessageBox.Show(exceptionObject.ToString());
+            }
+        }
+    }
+}
